291d7d879ca17e240abf3c2991ac7cdf2cd21122
Karum Ghag <karumghag09@gmail.com>
2025-03-09 15:47:45 +0000

    new player art, tracks direction you are facing

--- Player/player.gd
+++ Player/player.gd
@@ -2,22 +2,46 @@ extends CharacterBody2D
 
 class_name Player
 
+var mousePosition : Vector2
+
 @export_subgroup("Stats")
 @export var speed : float = 600
 @export var accel : float = 20
 var direction : Vector2
 var steering : Vector2
-var mass : float = 3
+@export var mass : float = 3
 
 
 @export_subgroup("Visuals")
 @export var animator : AnimationPlayer
+@export var otherAnimator : AnimationPlayer
 
 @export_subgroup("Bones")
+@export var skeleton : Skeleton2D
 @export var rightArm : Bone2D
 @export var leftArm : Bone2D
 
+#Getting direction to face
+enum facing {
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+}
+
+var currentFacing : facing
+var directionVectors : Array[Vector2] = [Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN]
+var dotProducts : Array[float] = [0, 0, 0, 0]
+var biggestDotIndex : int
+var directionToMouse : Vector2
+
+
+@export_subgroup("Debug")
+@export var debugText : Label
+
 func _process(_delta) -> void:
+
+	mousePosition = get_global_mouse_position()
 
 	direction = Input.get_vector("left", "right", "up", "down")
 
@@ -30,4 +54,53 @@ func _process(_delta) -> void:
 
 	rightArm.look_at(get_global_mouse_position())
 
+	if direction:
+		animator.play("Walk")
+		otherAnimator.play("FrontWalk")
+		
+	else:
+		animator.play("Idle")
+		otherAnimator.play("Idle")
+	
+
+	if get_global_mouse_position().x < global_position.x:
+		skeleton.scale.x = -1
+	else:
+		skeleton.scale.x = 1
+
+
+	directionToMouse = (global_position.direction_to(mousePosition)).normalized()
+	for i in range(len(dotProducts)):
+		dotProducts[i] = directionToMouse.dot(directionVectors[i])
+	
+	biggestDotIndex = getLargest(dotProducts)
+
+	if biggestDotIndex == 0:
+		currentFacing = facing.RIGHT
+	elif biggestDotIndex == 1:
+		currentFacing = facing.LEFT
+	elif biggestDotIndex == 2:
+		currentFacing = facing.UP
+	elif biggestDotIndex == 3:
+		currentFacing = facing.DOWN
+
+
+	debugText.text = "To Mouse: " + str(directionToMouse) + "\nLargest dot: " + str(biggestDotIndex) + "\nDots: " + str(dotProducts) + "\nDirection: " + str(facing.keys()[currentFacing])
+
+
+
+
+
 	move_and_slide()
+
+func getLargest(arr : Array[float]) -> int:
+	var highest : float = -1
+	var index : int = -1
+
+	for i in range(len(arr)):
+		if arr[i] > highest:
+			highest = arr[i]
+			index = i
+	
+
+	return index
